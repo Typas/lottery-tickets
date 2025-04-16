3f5c25d28ce56b59b841a5a93d891a006604dfd0
Typas Liao <typascake@gmail.com>
2025-04-16 22:55:00 +0800

    add shuffled for checking, add some documents

--- src/tickets.rs
+++ src/tickets.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{BinaryHeap, HashMap},
+    collections::HashMap,
     hash::{Hash, RandomState},
 };
 
@@ -11,6 +11,7 @@ where
     K: Hash + Eq,
     for<'a> U: User<'a, Key = K>,
 {
+    shuffled: bool,
     // The users in a hash map, use .users() to get the result
     users: HashMap<K, U, S>,
     // The prizes, the lower the index, the higher the priority.
@@ -26,6 +27,7 @@ where
         Self {
             users: HashMap::new(),
             prizes: Vec::new(),
+            shuffled: false,
         }
     }
 
@@ -33,6 +35,7 @@ where
         Self {
             users: HashMap::with_capacity(cap),
             prizes: Vec::new(),
+            shuffled: false,
         }
     }
 }
@@ -47,6 +50,7 @@ where
         Self {
             users: HashMap::with_capacity_and_hasher(cap, hasher),
             prizes: Vec::new(),
+            shuffled: false,
         }
     }
 
@@ -54,13 +58,23 @@ where
         Self {
             users: HashMap::with_hasher(hasher),
             prizes: Vec::new(),
+            shuffled: false,
         }
     }
 
+    /// Check if the prize has been distributed.
+    pub fn shuffled(&self) -> bool {
+        self.shuffled
+    }
+
+    /// Add a user to the lottery.
+    /// When the keys collide, it would return the old user.
     pub fn add_user<'a>(&mut self, user: U) -> Option<U> {
         self.users.insert(user.key(), user)
     }
 
+    /// Set all the users in the lottery.
+    /// It is possible to have less user if the keys collide.
     pub fn set_users<'a, C>(&mut self, users: C)
     where
         C: IntoIterator<Item = U>,
@@ -69,10 +83,14 @@ where
         self.users = users.into_iter().map(|u| (u.key(), u)).collect();
     }
 
+    /// Add a prize to the lottery.
+    /// The prior added prize would be considered as bigger prize.
     pub fn add_prize(&mut self, prize: Prize) {
         self.prizes.push(prize);
     }
 
+    /// Set the whole prizes of the lottery.
+    /// The prize in position 0 would be the biggest prize, while the last prize would be the smallest prize.
     pub fn set_prizes<C>(&mut self, prizes: C)
     where
         C: IntoIterator<Item = Prize>,
@@ -81,21 +99,28 @@ where
         self.prizes = prizes.into_iter().collect();
     }
 
+    /// Returns the users.
     pub fn users(&self) -> std::collections::hash_map::Values<'_, K, U> {
         self.users.values()
     }
 
+    /// Returns the users, which are mutable.
     pub fn users_mut(&mut self) -> std::collections::hash_map::ValuesMut<'_, K, U> {
         self.users.values_mut()
     }
 
+    /// Shuffle the slots and distribute the prizes to the users.
     pub fn shuffle<R>(&mut self, rng: &mut R)
     where
         R: Rng,
     {
         use std::iter::repeat_n;
+        // Shuffle twice would cause double spend
+        if self.shuffled {
+            return;
+        }
 
-        // Shuffle multiple times, only dispatch best prize to the user.
+        // Shuffle the slots, only give the best prize to the user.
         // Once the user has prize, isolate it from the lottery.
         // The worst case of shuffling would be O(n),
         // and each shuffling would take O(n) time.
@@ -105,20 +130,34 @@ where
             let mut ret = Vec::from_iter(
                 self.users
                     .values()
-                    // FIXME
+                    // FIXME:
                     // I suspect insisting keys to be `Clone` be more straightforward and cheaper
                     // than triggering (potentially expensive) hash algorithm each time...?
                     // BUT
                     // there may be some hash algorithms that are expensive to calculate
                     // and expensive to clone...
                     .flat_map(|user| {
-                        // hack: calculate hash each time to circumvent lifetime of keys of the map
+                        // HACK: calculate hash each time to circumvent lifetime of keys of the map
                         repeat_n((), user.ticket_count()).map(|_| user.key())
                     }),
             );
             ret.shuffle(rng);
             ret
         };
+
+        // map the prize to user
+        // once the key-mapped user has been saturated, try next user
+        // For example, let's call `this.prizes[i]` `pi`, `this.users.keys()[i]` `ki`.
+        // `p0.count = 1`, `p1.count = 2`, `p2.count = 3`.
+        // We will have `prizes = [&p0, &p1, &p1, &p2, &p2, &p2]`
+        // and the tickets would be like `[k5, k4, k4, k3, k2, k5, k6, k7...]`.
+        // over iteration the mapping would be
+        // p0 -> u5
+        // p1 -> u4
+        // p1 -> u3 // skip u4, assuming each user would only hold 1 prize
+        // p2 -> u2
+        // p2 -> u6 // skip u5, same assumption
+        // p2 -> u7
         let mut prizes = self.prizes.iter().flat_map(|p| repeat_n(p, p.count()));
         let mut tickets = tickets_god_only_knows_which_user.into_iter();
         while let Some(prize) = prizes.next() {
@@ -128,39 +167,6 @@ where
                 }
             }
         }
+        self.shuffled = true;
     }
 }
-
-#[allow(dead_code)]
-#[derive(Debug, Clone, Copy)]
-struct PrizeCounter {
-    priority: usize,
-    count: usize,
-}
-
-#[allow(dead_code)]
-impl PrizeCounter {
-    fn new(priority: usize, count: usize) -> Self {
-        Self { priority, count }
-    }
-}
-
-impl Ord for PrizeCounter {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.priority.cmp(&other.priority)
-    }
-}
-
-impl PartialOrd for PrizeCounter {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.priority.partial_cmp(&other.priority)
-    }
-}
-
-impl PartialEq for PrizeCounter {
-    fn eq(&self, other: &Self) -> bool {
-        self.priority.eq(&other.priority)
-    }
-}
-
-impl Eq for PrizeCounter {}
