afd56a58ce5c7e0d579bbb77d6bee3ca3e72c773
Typas Liao <typascake@gmail.com>
2025-04-27 22:50:28 +0800

    use feature to control the benching catogory

--- benches/benches/multiple-prize.rs
+++ benches/benches/multiple-prize.rs
@@ -12,7 +12,7 @@ static ALLOCATOR: jemallocator::Jemalloc = jemallocator::Jemalloc;
 criterion_group!(
     name = benches;
     config = Criterion::default().noise_threshold(0.03);
-    targets = bench
+    targets = multiple_large_t_large_p, multiple_large_t_medium_p, multiple_large_t_small_p, multiple_medium_t_large_p, multiple_medium_t_medium_p, multiple_medium_t_medium_p_csprng, multiple_medium_t_small_p
 );
 criterion_main!(benches);
 
@@ -73,211 +73,222 @@ fn warm(time_coeff: u32) -> Duration {
     time_coeff * TIME_MEASUREMENT_BASE / WARM_UP_COEFF
 }
 
-pub fn bench(c: &mut Criterion) {
-    // bench function naming rule
-    // multiple_[number of tickets_t]_[number of prizes_p]_[Rng]
-    // Rng would be either Prng (default) or CsPrng
-    {
-        let mut g = c.benchmark_group("multiple_large_t_large_p");
-        let nr_ticket = large();
-        let nr_prize = large();
-        let time_coeff = 9;
-        g.measurement_time(measure(time_coeff))
-            .warm_up_time(warm(time_coeff));
-        g.bench_function("array", |b| {
-            let mut rng = Prng::seed_from_u64(1);
-            bench_iter_multiple!(array
-                b,
-                rng,
-                nr_prize,
-                1.max(nr_prize / NUM_ENTRANTS),
-                1.max(nr_ticket / NUM_ENTRANTS)
-            );
-        });
+// bench function naming rule
+// multiple_[number of tickets_t]_[number of prizes_p]_[Rng]
+// Rng would be either Prng (default) or CsPrng
+pub fn multiple_large_t_large_p(c: &mut Criterion) {
+    let mut g = c.benchmark_group("multiple_large_t_large_p");
+    let time_coeff = 9;
+    let nr_ticket = large();
+    let nr_prize = large();
+    g.measurement_time(measure(time_coeff)).warm_up_time(warm(time_coeff));
+    #[cfg(feature = "array")]
+    g.bench_function("array", |b| {
+        let mut rng = Prng::seed_from_u64(1);
+        bench_iter_multiple!(array
+                             b,
+                             rng,
+                             nr_prize,
+                             1.max(nr_prize / NUM_ENTRANTS),
+                             1.max(nr_ticket / NUM_ENTRANTS)
+        );
+    });
 
-        g.bench_function("tree", |b| {
-            let mut rng = Prng::seed_from_u64(1);
-            bench_iter_multiple!(tree
-                b,
-                rng,
-                nr_prize,
-                1.max(nr_prize / NUM_ENTRANTS),
-                1.max(nr_ticket / NUM_ENTRANTS)
-            );
-        });
-    }
-    {
-        let mut g = c.benchmark_group("multiple_large_t_medium_p");
-        let nr_ticket = large();
-        let nr_prize = medium();
-        let time_coeff = 2;
-        g.measurement_time(measure(time_coeff))
-            .warm_up_time(warm(time_coeff));
-        g.bench_function("array", |b| {
-            let mut rng = Prng::seed_from_u64(1);
-            bench_iter_multiple!(array
-                b,
-                rng,
-                nr_prize,
-                1.max(nr_prize / NUM_ENTRANTS),
-                1.max(nr_ticket / NUM_ENTRANTS)
-            );
-        });
+    #[cfg(feature = "tree")]
+    g.bench_function("tree", |b| {
+        let mut rng = Prng::seed_from_u64(1);
+        bench_iter_multiple!(tree
+                             b,
+                             rng,
+                             nr_prize,
+                             1.max(nr_prize / NUM_ENTRANTS),
+                             1.max(nr_ticket / NUM_ENTRANTS)
+        );
+    });
+}
 
-        g.bench_function("tree", |b| {
-            let mut rng = Prng::seed_from_u64(1);
-            bench_iter_multiple!(tree
-                b,
-                rng,
-                nr_prize,
-                1.max(nr_prize / NUM_ENTRANTS),
-                1.max(nr_ticket / NUM_ENTRANTS)
-            );
-        });
-    }
-    {
-        let mut g = c.benchmark_group("multiple_large_t_small_p");
-        let nr_ticket = large();
-        let nr_prize = small();
-        let time_coeff = 1;
-        g.measurement_time(measure(time_coeff))
-            .warm_up_time(warm(time_coeff));
-        g.bench_function("array", |b| {
-            let mut rng = Prng::seed_from_u64(1);
-            bench_iter_multiple!(array
-                b,
-                rng,
-                nr_prize,
-                1.max(nr_prize / NUM_ENTRANTS),
-                1.max(nr_ticket / NUM_ENTRANTS)
-            );
-        });
+pub fn multiple_large_t_medium_p(c: &mut Criterion) {
+    let mut g = c.benchmark_group("multiple_large_t_medium_p");
+    let nr_ticket = large();
+    let nr_prize = medium();
+    let time_coeff = 2;
+    g.measurement_time(measure(time_coeff)).warm_up_time(warm(time_coeff));
+    #[cfg(feature = "array")]
+    g.bench_function("array", |b| {
+        let mut rng = Prng::seed_from_u64(1);
+        bench_iter_multiple!(array
+            b,
+            rng,
+            nr_prize,
+            1.max(nr_prize / NUM_ENTRANTS),
+            1.max(nr_ticket / NUM_ENTRANTS)
+        );
+    });
 
-        g.bench_function("tree", |b| {
-            let mut rng = Prng::seed_from_u64(1);
-            bench_iter_multiple!(tree
-                b,
-                rng,
-                nr_prize,
-                1.max(nr_prize / NUM_ENTRANTS),
-                1.max(nr_ticket / NUM_ENTRANTS)
-            );
-        });
-    }
-    {
-        let mut g = c.benchmark_group("multiple_medium_t_large_p");
-        let nr_ticket = medium();
-        let nr_prize = large();
-        let time_coeff = 4;
-        g.measurement_time(measure(time_coeff))
-            .warm_up_time(warm(time_coeff));
-        g.bench_function("array", |b| {
-            let mut rng = Prng::seed_from_u64(1);
-            bench_iter_multiple!(array
-                b,
-                rng,
-                nr_prize,
-                1.max(nr_prize / NUM_ENTRANTS),
-                1.max(nr_ticket / NUM_ENTRANTS)
-            );
-        });
+    #[cfg(feature = "tree")]
+    g.bench_function("tree", |b| {
+        let mut rng = Prng::seed_from_u64(1);
+        bench_iter_multiple!(tree
+            b,
+            rng,
+            nr_prize,
+            1.max(nr_prize / NUM_ENTRANTS),
+            1.max(nr_ticket / NUM_ENTRANTS)
+        );
+    });
+}
 
-        g.bench_function("tree", |b| {
-            let mut rng = Prng::seed_from_u64(1);
-            bench_iter_multiple!(tree
-                b,
-                rng,
-                nr_prize,
-                1.max(nr_prize / NUM_ENTRANTS),
-                1.max(nr_ticket / NUM_ENTRANTS)
-            );
-        });
-    }
-    {
-        let mut g = c.benchmark_group("multiple_medium_t_medium_p");
-        let nr_ticket = medium();
-        let nr_prize = medium();
-        let time_coeff = 1;
-        g.measurement_time(measure(time_coeff))
-            .warm_up_time(warm(time_coeff));
-        g.bench_function("array", |b| {
-            let mut rng = Prng::seed_from_u64(1);
-            bench_iter_multiple!(array
-                b,
-                rng,
-                nr_prize,
-                1.max(nr_prize / NUM_ENTRANTS),
-                1.max(nr_ticket / NUM_ENTRANTS)
-            );
-        });
+pub fn multiple_large_t_small_p(c: &mut Criterion) {
+    let mut g = c.benchmark_group("multiple_large_t_small_p");
+    let nr_ticket = large();
+    let nr_prize = small();
+    let time_coeff = 1;
+    g.measurement_time(measure(time_coeff)).warm_up_time(warm(time_coeff));
+    #[cfg(feature = "array")]
+    g.bench_function("array", |b| {
+        let mut rng = Prng::seed_from_u64(1);
+        bench_iter_multiple!(array
+            b,
+            rng,
+            nr_prize,
+            1.max(nr_prize / NUM_ENTRANTS),
+            1.max(nr_ticket / NUM_ENTRANTS)
+        );
+    });
 
-        g.bench_function("tree", |b| {
-            let mut rng = Prng::seed_from_u64(1);
-            bench_iter_multiple!(tree
-                b,
-                rng,
-                nr_prize,
-                1.max(nr_prize / NUM_ENTRANTS),
-                1.max(nr_ticket / NUM_ENTRANTS)
-            );
-        });
-    }
-    {
-        let mut g = c.benchmark_group("multiple_medium_t_medium_p_csprng");
-        let nr_ticket = medium();
-        let nr_prize = medium();
-        let time_coeff = 1;
-        g.measurement_time(measure(time_coeff))
-            .warm_up_time(warm(time_coeff));
-        g.bench_function("array", |b| {
-            let mut rng = CsPrng::seed_from_u64(1);
-            bench_iter_multiple!(array
-                b,
-                rng,
-                nr_prize,
-                1.max(nr_prize / NUM_ENTRANTS),
-                1.max(nr_ticket / NUM_ENTRANTS)
-            );
-        });
+    #[cfg(feature = "tree")]
+    g.bench_function("tree", |b| {
+        let mut rng = Prng::seed_from_u64(1);
+        bench_iter_multiple!(tree
+            b,
+            rng,
+            nr_prize,
+            1.max(nr_prize / NUM_ENTRANTS),
+            1.max(nr_ticket / NUM_ENTRANTS)
+        );
+    });
+}
 
-        g.bench_function("tree", |b| {
-            let mut rng = CsPrng::seed_from_u64(1);
-            bench_iter_multiple!(tree
-                b,
-                rng,
-                nr_prize,
-                1.max(nr_prize / NUM_ENTRANTS),
-                1.max(nr_ticket / NUM_ENTRANTS)
-            );
-        });
-    }
-    {
-        let mut g = c.benchmark_group("multiple_medium_t_small_p");
-        let nr_ticket = medium();
-        let nr_prize = small();
-        let time_coeff = 1;
-        g.measurement_time(measure(time_coeff))
-            .warm_up_time(warm(time_coeff));
-        g.bench_function("array", |b| {
-            let mut rng = Prng::seed_from_u64(1);
-            bench_iter_multiple!(array
-                b,
-                rng,
-                nr_prize,
-                1.max(nr_prize / NUM_ENTRANTS),
-                1.max(nr_ticket / NUM_ENTRANTS)
-            );
-        });
+pub fn multiple_medium_t_large_p(c: &mut Criterion) {
+    let mut g = c.benchmark_group("multiple_medium_t_large_p");
+    let nr_ticket = medium();
+    let nr_prize = large();
+    let time_coeff = 4;
+    g.measurement_time(measure(time_coeff)).warm_up_time(warm(time_coeff));
+    #[cfg(feature = "array")]
+    g.bench_function("array", |b| {
+        let mut rng = Prng::seed_from_u64(1);
+        bench_iter_multiple!(array
+            b,
+            rng,
+            nr_prize,
+            1.max(nr_prize / NUM_ENTRANTS),
+            1.max(nr_ticket / NUM_ENTRANTS)
+        );
+    });
 
-        g.bench_function("tree", |b| {
-            let mut rng = Prng::seed_from_u64(1);
-            bench_iter_multiple!(tree
-                b,
-                rng,
-                nr_prize,
-                1.max(nr_prize / NUM_ENTRANTS),
-                1.max(nr_ticket / NUM_ENTRANTS)
-            );
-        });
-    }
+    #[cfg(feature = "tree")]
+    g.bench_function("tree", |b| {
+        let mut rng = Prng::seed_from_u64(1);
+        bench_iter_multiple!(tree
+            b,
+            rng,
+            nr_prize,
+            1.max(nr_prize / NUM_ENTRANTS),
+            1.max(nr_ticket / NUM_ENTRANTS)
+        );
+    });
+}
+
+pub fn multiple_medium_t_medium_p(c: &mut Criterion) {
+    let mut g = c.benchmark_group("multiple_medium_t_medium_p");
+    let nr_ticket = medium();
+    let nr_prize = medium();
+    let time_coeff = 1;
+    g.measurement_time(measure(time_coeff)).warm_up_time(warm(time_coeff));
+    #[cfg(feature = "array")]
+    g.bench_function("array", |b| {
+        let mut rng = Prng::seed_from_u64(1);
+        bench_iter_multiple!(array
+            b,
+            rng,
+            nr_prize,
+            1.max(nr_prize / NUM_ENTRANTS),
+            1.max(nr_ticket / NUM_ENTRANTS)
+        );
+    });
+
+    #[cfg(feature = "tree")]
+    g.bench_function("tree", |b| {
+        let mut rng = Prng::seed_from_u64(1);
+        bench_iter_multiple!(tree
+            b,
+            rng,
+            nr_prize,
+            1.max(nr_prize / NUM_ENTRANTS),
+            1.max(nr_ticket / NUM_ENTRANTS)
+        );
+    });
+}
+
+pub fn multiple_medium_t_medium_p_csprng(c: &mut Criterion) {
+    let mut g = c.benchmark_group("multiple_medium_t_medium_p_csprng");
+    let nr_ticket = medium();
+    let nr_prize = medium();
+    let time_coeff = 1;
+    g.measurement_time(measure(time_coeff)).warm_up_time(warm(time_coeff));
+    #[cfg(feature = "array")]
+    g.bench_function("array", |b| {
+        let mut rng = CsPrng::seed_from_u64(1);
+        bench_iter_multiple!(array
+            b,
+            rng,
+            nr_prize,
+            1.max(nr_prize / NUM_ENTRANTS),
+            1.max(nr_ticket / NUM_ENTRANTS)
+        );
+    });
+
+    #[cfg(feature = "tree")]
+    g.bench_function("tree", |b| {
+        let mut rng = CsPrng::seed_from_u64(1);
+        bench_iter_multiple!(tree
+            b,
+            rng,
+            nr_prize,
+            1.max(nr_prize / NUM_ENTRANTS),
+            1.max(nr_ticket / NUM_ENTRANTS)
+        );
+    });
+}
+
+pub fn multiple_medium_t_small_p(c: &mut Criterion) {
+    let mut g = c.benchmark_group("multiple_medium_t_small_p");
+    let nr_ticket = medium();
+    let nr_prize = small();
+    let time_coeff = 1;
+    g.measurement_time(measure(time_coeff)).warm_up_time(warm(time_coeff));
+    #[cfg(feature = "array")]
+    g.bench_function("array", |b| {
+        let mut rng = Prng::seed_from_u64(1);
+        bench_iter_multiple!(array
+            b,
+            rng,
+            nr_prize,
+            1.max(nr_prize / NUM_ENTRANTS),
+            1.max(nr_ticket / NUM_ENTRANTS)
+        );
+    });
+
+    #[cfg(feature = "tree")]
+    g.bench_function("tree", |b| {
+        let mut rng = Prng::seed_from_u64(1);
+        bench_iter_multiple!(tree
+            b,
+            rng,
+            nr_prize,
+            1.max(nr_prize / NUM_ENTRANTS),
+            1.max(nr_ticket / NUM_ENTRANTS)
+        );
+    });
 }
